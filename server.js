3c6593bd491e721a43bb049ef53dfe1f19cb947b
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-07-09 12:06:23 +0300

    Connect server to db

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const dns = require("node:dns");
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const dotenv = require("dotenv").config(); // making .env file available
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// Connect to MongoDB using connection string in .env
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`);
+});
+
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
